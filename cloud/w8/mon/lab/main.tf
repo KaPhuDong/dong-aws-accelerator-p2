87c1a936f0120e92380a85ba59de818a1be49b5d
Ka Phu Dong <175317472+KaPhuDong@users.noreply.github.com>
2026-06-02 16:30:29 +0700

    [W8-D1] terraform ec2 lab

---
+++ cloud/w8/mon/lab/main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_key_pair" "udemy-keypair" {
+  key_name   = "udemy-keypair"
+  public_key = file("./keypair/udemy-key.pub")
+}
+
+resource "aws_instance" "demo-instance" {
+  ami           = "ami-0543dbdaf4e114be7"
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.udemy-keypair.key_name
+  tags = {
+    Name = "Udemy Demo"
+    Company = "XBrain"
+    Domain = "DevOps"
+  }
+  vpc_security_group_ids = [aws_security_group.test-security-group.id]
+}
+
+resource "aws_security_group" "test-security-group" {
+  name        = "test-security-group"
+  description = "test-security-group"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
